e4c4f3f233d86282a2827e7cbcb22e464f60d315
dawood oballi <d4w00d@hotmail.com>
2020-09-30 17:20:39 +0100

    Downloaded necessary packages. Reverse Engineered the DB into models (Database First)

---
+++ IP_Booking_Overtime/IndividualProject_DatabaseContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IP_Booking_Overtime
+{
+    public partial class IndividualProject_DatabaseContext : DbContext
+    {
+        public IndividualProject_DatabaseContext()
+        {
+        }
+
+        public IndividualProject_DatabaseContext(DbContextOptions<IndividualProject_DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=IndividualProject_Database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__Users__1788CCAC31E05592");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.Property(e => e.UserName).HasMaxLength(40);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ IP_Booking_Overtime/Users.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IP_Booking_Overtime
+{
+    public partial class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
