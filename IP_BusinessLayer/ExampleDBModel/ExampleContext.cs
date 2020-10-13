40d1fc829e775272ee356953a0a700482fccabe0
dawood oballi <d4w00d@hotmail.com>
2020-10-13 15:14:33 +0100

    Created example context for mocking

---
+++ IP_BusinessLayer/ExampleDBModel/ExampleContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IP_Booking_Overtime.ExampleDBModel
+{
+    public class ExampleContext : DbContext
+    {
+        public ExampleContext() { }
+
+        public ExampleContext(DbContextOptions<ExampleContext> options): base(options)
+        { }
+
+        public virtual DbSet<Users> Users { get; set; }
+    }
+}
---
+++ IP_BusinessLayer/ExampleDBModel/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IP_BusinessLayer.ExampleDBModel
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserId} {UserName}";
+        }
+    }
+}
